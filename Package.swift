04eb9116e9a8407fbbe903f89830a097dce18462
Alvin <12960590+alvinh77@users.noreply.github.com>
2025-02-23 17:30:47 +1100

    First commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ios-cache",
+    products: [
+        .library(
+            name: "Cache",
+            targets: ["Cache"]
+        ),
+    ],
+    targets: [
+        .target(name: "Cache"),
+        .testTarget(
+            name: "CacheTests",
+            dependencies: [
+                .target(name: "Cache")
+            ]
+        ),
+    ]
+)
